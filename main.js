18cbed9357d44baacc5d96f5d9e0b706b3656380
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-09-27 04:36:06 +0100

    promise application

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
 // promises continues
 
-let promise = new Promise(function(resolve, reject){
-    throw new Error("error")
-})
+// let promise = new Promise(function(resolve, reject){
+//     throw new Error("error")
+// })
 
-.finally(() => console.log("promise is ready"))
-.then(result => console.log(result))
+// .finally(() => console.log("promise is ready"))
+// .then(result => console.log(result))
+
+
+
+// lets have a call back function
+
+// function loadScript(src, callback){
+//     let script = document.createElement('script')
+//     script.src = src
+
+//     script.onload = () => callback(null, script)
+//     script.onerror = () => callback(new Error(`script load error for ${src}`))
+
+//     document.head.append(script)
+// }
+
+// promise version
+
+function loadScript(src){
+    return new Promise(function(resolve,reject){
+        let script = document.createElement('script')
+        script.src = src
+
+        script.onload =() => resolve(script)
+        script.onerror=() => reject(new Error(`script load error for ${src}`))
+
+        document.head.append(script)
+    })
+}
+
+let promise = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js")
+promise.then(
+    script => alert(`the script ${script.src} is loaded`),
+    error => alert(`Error: ${error.message}`)
+)
+
+promise.then(
+    script => alert('Another handler...')
+)
