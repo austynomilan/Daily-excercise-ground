6f8d88b8145c4d22e2b0ffe2c8c44d85cdd7317d
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-10-03 14:21:56 +0100

    animated circle

--- main.js
+++ main.js
@@ -23,38 +23,62 @@
 
 // promise version
 
-function loadScript(src){
-    return new Promise(function(resolve,reject){
-        let script = document.createElement('script')
-        script.src = src
+// function loadScript(src){
+//     return new Promise(function(resolve,reject){
+//         let script = document.createElement('script')
+//         script.src = src
 
-        script.onload =() => resolve(script)
-        script.onerror=() => reject(new Error(`script load error for ${src}`))
+//         script.onload =() => resolve(script)
+//         script.onerror=() => reject(new Error(`script load error for ${src}`))
 
-        document.head.append(script)
-    })
-}
+//         document.head.append(script)
+//     })
+// }
 
-let promise = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js")
-promise.then(
-    result => alert(`the script ${result.src} is loaded`),
-    error => alert(`Error: ${error.message}`)
-)
+// let promise = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js")
+// promise.then(
+//     result => alert(`the script ${result.src} is loaded`),
+//     error => alert(`Error: ${error.message}`)
+// )
 
-promise.then(
-    script => alert('Another handler...')
-)
-
-
-// delay with a promise
-
-function delay(ms){
-return new Promise(resolve => setTimeout(resolve, ms))    
-}
-
-delay(3000).then(() => alert('runs after 3 seconds'));
+// promise.then(
+//     script => alert('Another handler...')
+// )
 
 
+// // delay with a promise
+
+// function delay(ms){
+// return new Promise(resolve => setTimeout(resolve, ms))    
+// }
+
+// delay(3000).then(() => alert('runs after 3 seconds'));
+
+
+//animated circle
 function go(){
-    
+ showCircle(150, 150, 100, div =>{
+    div.classList.add('message-ball')
+    div.append('Hello, world!')
+ })
+}
+
+function showCircle(cx, cy, radius, callback){
+    let div = document.createElement('div')
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px'
+    div.style.right = cy + 'px'
+    div.className = 'circle'
+    document.body.append(div)
+
+    setTimeout(()=> {
+        div.style.width = radius * 2 + 'px'
+        div.style.height = radius * 2 + 'px'
+
+        div.addEventListener('transitionend', function handler(){
+            div.removeEventListener('transitionend', handler)
+            callback(div)
+        })
+    }, 0)
 }
